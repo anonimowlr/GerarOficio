f8d8cf285f8ee9f63b10aba5a886c4b0eda7d268
jocimarwlr <jocimarwlr@gmail.com>
2019-02-03 10:29:05 -0200

    Comitado

--- src/DAO/Conexao.java
+++ src/DAO/Conexao.java
@@ -34,10 +34,10 @@ public class Conexao {
     this.driver = "com.mysql.jdbc.Driver"; //Classe do driver JDBC
     this.banco = "bdjudicial"; ///servidor
         if ("local".equals(usarBanco)) {//Para gravar local-deixar apenas "local" na condição
-            this.host = "192.168.1.103"; //ip do banco de dados
-            this.usuario = "jocimar";
-            this.senha = "jwalter1983";
-            this.url = "jdbc:mysql://" + host + ":4040/" + banco; //URL de conexão
+            this.host = "localhost"; //ip do banco de dados
+            this.usuario = "root";
+            this.senha = "root";
+            this.url = "jdbc:mysql://" + host + ":3306/" + banco; //URL de conexão
         }else{
             this.host = "10.105.87.250";
             this.usuario = "basso";  
